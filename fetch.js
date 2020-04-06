c1bbe9dfd3a69e380570689523ebb9d28edf3f52
Gordon Preumont <preumontgr@gmail.com>
2020-04-06 13:23:00 +0200

    fetch simpson

---
+++ fetch.js
@@ -0,0 +1,22 @@
+const getQuote = () => {
+    return axios({
+        "method": "GET",
+        "url": "https://simpsons-quotes-api.herokuapp.com/quotes",
+        }
+    ).then(
+        response => response.data[0]
+    ).then(
+        simpson => {
+            const simpsonHTML = `
+                <h3>${simpson.character}</h3>
+                <p>${simpson.quote}</p>
+                <img src="${simpson.image}" alt=${simpson.character}>
+            `;
+            document.getElementById('simpson').innerHTML = simpsonHTML;
+        }
+    )
+}
+
+
+getQuote()
+
